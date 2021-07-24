2a1f9084f6754fe9eb9b12579340f2dfe4dceb02
arthur-adjedj <aarthuur01@gmail.com>
2021-07-24 16:41:34 +0200

    corrected recip and lexical_order + added a test

--- Ocaml/représentation groupe abélien fini.ml
+++ Ocaml/représentation groupe abélien fini.ml
@@ -16,60 +16,55 @@ let lexical_order m (i,j) = m*i+j;;
 
 let reciproque_order m k = (k/m,k mod m);;
 
-(*calcule les [|n|] à chaque étape du calcul*)
-let sums arr =
-	let n = Array.length arr in
-    let res = Array.make n 0 in
-    res.(0) <- arr.(0);
-    for i=1 to n-1 do
-    	res.(i) <- res.(i-1)*arr.(i)
-    done;
-    res;;
+
+let lex_order arr l = 
+    let rec aux l i = 
+    match l with
+        |[] -> failwith "empty set"
+        |[x] -> x
+            |n::m::r2 -> aux ((lexical_order arr.(i+1) (n,m))::r2) (i+1)
+    in aux l 0;;
 
 
-let lex_order arr x = 
-    let n = Array.length arr in
-    let res = ref (lexical_order arr.(1) (x.(0),x.(1))) in
-    for i=1 to n-2 do
-    	res:= lexical_order arr.(i+1) (!res,x.(i+1))
-    done;
-    !res mod n;;
+let l_o_t (a,b) = (*print_int a; print_newline ();*) [b;a];;
 
 let recip arr x =
-	let n = Array.length arr in
-    if n=1 then [|x|]
+    let n = Array.length arr in
+    if n=1 then [x]
     else begin
-        let res = Array.make n 0 in
-        let (a1,b1) = reciproque_order arr.(n-1) x in
-        let a = ref a1 and b = ref b1 in
-        for i=0 to n-2 do
-        	res.(n-i-1) <- !b;
-            if i<> n-2 then (
-            	let new1  = reciproque_order arr.(n-2-i) !a in
-            	a := fst new1;
-            	b := snd new1 )
-        done;
-        res.(0) <- !a;
-        res
-        end;;
+        let rec aux y i =
+            if i=2 then l_o_t (reciproque_order arr.(1) y)
+            else begin
+                 let (a,b) = reciproque_order arr.(i-1) y in
+                    (*print_int a; print_newline ();*)
+                    b::(aux a (i-1))
+                end
+        in List.rev (aux x n)
+    end;;
 
 let op t1 t2 arr = 
-	let n = Array.length t1 in
-    let t = Array.make n 0 in
-    for i = 0 to n-1 do
-    	t.(i) <- (t1.(i) + t2.(i)) mod arr.(i)
-    done;
-    t;;
+    let rec aux l1 l2 i = match l1, l2 with 
+        |[],[] -> []
+        |h1::r1,h2::r2 -> (h1 + h2 mod arr.(i))::(aux r1 r2 (i+1))
+        |_ -> failwith "error"
+    in aux t1 t2 0
+
     
 let loi arr i j = lex_order arr (op (recip arr i) (recip arr j) arr);;
 
 let opbis t1 t2 arr = 
-	let n = Array.length t1 in
-    let t = Array.make n 0 in
-    for i = 0 to n-1 do
-    	t.(i) <- ((((t2.(i) - t1.(i)) ) mod arr.(i)) + arr.(i)) mod arr.(i);
-    done;
-    t;;
+    let rec aux l1 l2 i = match l1, l2 with 
+        |[],[] -> []
+        |h1::r1,h2::r2 -> (((h2 + h1) mod arr.(i))+ arr.(i) mod arr.(i))::(aux r1 r2 (i+1))
+        |_ -> failwith "error"
+    in aux t1 t2 0
+
 
 let abelian_epsilon arr (i,j) = lex_order arr (opbis (recip arr i) (recip arr j) arr);;
+
+
+let rec check arr n i = if i=(n-1) then lex_order arr (recip arr (n-1)) = (n-1)
+    else (lex_order arr (recip arr i) = i) && (check arr n (i+1))
+
+let%test "bij" = check [|4;5|] 20 0
 
