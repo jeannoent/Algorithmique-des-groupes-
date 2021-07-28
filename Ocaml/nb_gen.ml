82183dd1af2c2d7de6dab867d47bd4baeebd32da
jeannoent <jean.chessa@laposte.net>
2021-07-28 22:25:17 +0200

    LE OUAIBE

--- Ocaml/nb_gen.ml
+++ Ocaml/nb_gen.ml
@@ -148,3 +148,4 @@ let abelian_gen_mod arr =
 
 let pi_symetric n = nb_gen n (sym_epsilon n);;       
    
+let pi_cyclic n = nb_gen n (znz n);;
--- main.ml
+++ main.ml
@@ -6,4 +6,8 @@ let _ =
   Js.Unsafe.global##.ocamlcallback := (object%js
         val abelian = Js.wrap_meth_callback
             (fun _ x -> abelian_gen (Js.to_array x))
+        val cyclic = Js.wrap_meth_callback
+            (fun _ x -> pi_cyclic(x))
+        val symetric = Js.wrap_meth_callback
+            (fun _ x -> pi_symetric(x))
       end);
