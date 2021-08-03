8c4908dbb2482d3444e6618dde8141786824ce5d
jeannoent <jean.chessa@laposte.net>
2021-08-03 12:06:21 +0200

    oups mdr

--- lib_gen/nb_gen.ml
+++ lib_gen/nb_gen.ml
@@ -125,4 +125,4 @@ let pi_cyclic n = nb_gen n (znz n);;
 
 let pi_diedral n = nb_gen n (died_epsilon n);;
 
-let pi_inv n = nb_gen n (epsilon_star n);;
+let pi_inv n = nb_gen (euler n) (epsilon_star n);;
