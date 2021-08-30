fe3278c1c88721cd3b3e992b8b404d56b6fcb8ac
jeannoent <jean.chessa@laposte.net>
2021-08-30 23:30:03 +0200

    added pow_mod back for performance reasons

--- lib_gen/loi_star.ml
+++ lib_gen/loi_star.ml
@@ -1,5 +1,3 @@
-open Big_int
-
 
 let rec pgcd a b = match b with
 |0 -> a
@@ -25,12 +23,18 @@ let starToInt n i =
 
 let mult_mod_n n (a,b) = (a*b) mod n
 
+(*returne a^n mod m*)    
+let rec pow_mod a n m = match n with
+  | 0 -> 1
+  | 1 -> a
+  | _ -> 
+    let b = pow_mod a (n / 2) m mod m in
+    (b * b mod m)* (if n mod 2 = 0 then 1 else a mod m ) mod m;;
+
 
 let inv n k = 
 	let p = euler n in
-	let r = power_int_positive_int k (p-1) in
-    int_of_big_int (mod_big_int r (big_int_of_int n))
-
+    pow_mod k (p-1) n
 
 let intToStar n i =
 	let r = ref 0 in
