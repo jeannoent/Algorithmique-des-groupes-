bac1c43f09ec6fa9c1b3c0c570b3bb283eafbb6e
jbdoderlein <jean.baptiste.doderlein@gmail.com>
2022-04-16 13:27:04 +0200

    Add diedral

--- main.ml
+++ main.ml
@@ -7,27 +7,27 @@ open Lib_gen.Decomposition_et_rang
 let _ = 
   Js.Unsafe.global##.ocamlcallback := (object%js
         val abelianorder = Js.wrap_meth_callback
-            (fun _ x -> Lib_gen.Loi_gp_ab.order (Js.to_array x)) (* Partie ordre abelian : Done *)
+            (fun _ x -> Lib_gen.Loi_gp_ab.order (Js.to_array x))
         val abelian = Js.wrap_meth_callback
-            (fun _ x -> pi_abelian (Js.to_array x)) (* Partie gen groupe abelien : Done *)
+            (fun _ x -> pi_abelian (Js.to_array x))
         val symetric = Js.wrap_meth_callback
-            (fun _ x -> pi_symetric(x)) (* Partie gen groupe symetrique : Nope *)
+            (fun _ x -> pi_symetric(x))
         val diedral = Js.wrap_meth_callback
             (fun _ x -> pi_diedral(x)) (* Partie gen groupe diedral : Nope *)
         val star = Js.wrap_meth_callback
             (fun _ x -> pi_star(x)) (* Partie gen groupe star : Nope *)
         val abeliang = Js.wrap_meth_callback
-            (fun _ x -> pi_abelian_graph (Js.to_array x)) (* Partie gen graphe abelien : Nope *)
+            (fun _ x -> pi_abelian_graph (Js.to_array x))
         val symetricg = Js.wrap_meth_callback
-            (fun _ x -> pi_symetric_graph(x)) (* Partie gen graphe symetrique : Nope *)
+            (fun _ x -> pi_symetric_graph(x))
         val diedralg = Js.wrap_meth_callback
-            (fun _ x -> pi_diedral_graph(x)) (* Partie gen graphe diedral : Nope *)
+            (fun _ x -> pi_diedral_graph(x))
         val starg = Js.wrap_meth_callback
-            (fun _ x -> pi_star_graph(x)) (* Partie gen graphe star : Nope *)
+            (fun _ x -> pi_star_graph(x))
         val rabelian = Js.wrap_meth_callback
-            (fun _ x -> abelian_rg (Js.to_array x)) (* Partie rang abelien : Done *)
+            (fun _ x -> abelian_rg (Js.to_array x))
         val rsymetric = Js.wrap_meth_callback
-            (fun _ x -> sym_rg(x)) (* Partie rang symetrique : Nope *)
+            (fun _ x -> sym_rg(x))
         val rdiedral = Js.wrap_meth_callback
             (fun _ x -> died_rg(x)) (* Partie rang diedral : Nope *)
         val rstar = Js.wrap_meth_callback
